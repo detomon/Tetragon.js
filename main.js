c4002abe7cdd9ac158a0d1f9674f2c88026fbe2c
detomon <simon@monoxid.ch>
2015-09-28 10:35:29 +0200

    Implement example

--- main.js
+++ main.js
@@ -3,13 +3,22 @@
 
 var canvas = document.getElementById('canvas');
 
+var r  = 50;
+var a  = 0;
+var ad = 90 / 180 * Math.PI;
+
 var controller = new T.Canvas({
 	element: canvas,
 	tick: function (dt, info) {
-
+		a += ad * dt;
+		a %= Math.PI * 2;
 	},
 	draw: function (ctx, info) {
-		ctx.fillRect(0, 0, 20, 20);
+		var viewport = info.viewport.size;
+		var x = Math.cos(a) * r + viewport.x * 0.5;
+		var y = Math.sin(a) * r + viewport.y * 0.5;
+
+		ctx.fillRect(x, y, 20, 20);
 	}
 });
 
