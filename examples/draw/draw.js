6debbcc675c5a6016cfd3ca37096dea3dcf7fe5c
detomon <simon@monoxid.ch>
2015-12-01 15:20:48 +0100

    Add new simple example

---
+++ examples/draw/draw.js
@@ -0,0 +1,29 @@
+(function (T) {
+'use strict';
+
+var r = 0;
+
+var canvas = document.getElementById('canvas');
+
+var controller = new T.Canvas({
+	element: canvas,
+	tick: function (dt, info) {
+		// 90° / sec
+		r += 90 * (Math.PI / 180) * dt;
+	},
+	draw: function (ctx, info) {
+		var x = Math.cos(r) * 100;
+		var y = Math.sin(r) * 100;
+
+		ctx.fillRect(x, y, 10, 10);
+	}
+});
+
+/*var mat = controller.transform.translate(new T.Vector(-200, -200));
+controller.transform = mat;
+
+console.log(mat);*/
+
+controller.startAnimating();
+
+}(Tetragon));
