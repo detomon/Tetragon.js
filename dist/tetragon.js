1122b378b92a4cd3f80d891ea198fd81c15faadb
detomon <simon@monoxid.ch>
2016-02-05 17:08:07 +0100

    Update dist files

--- dist/tetragon.js
+++ dist/tetragon.js
@@ -1137,9 +1137,6 @@ proto.offsetFromEvent = function (e) {
 		offset.y = e.changedTouches[0].pageY - offset.y;
 	}
 
-	offset.x -= this.element.offsetWidth * 0.5;
-	offset.y -= this.element.offsetHeight * 0.5;
-
 	offset = offset.mult(1.0 / scale);
 
 	return offset;
